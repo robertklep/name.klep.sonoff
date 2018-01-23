3f3b07b358e5d8fdf0b5e7001695b38d1ae1626c
Robert Klep <robert@klep.name>
2018-01-23 15:03:10 +0100

    Removed unused code

--- drivers/sonoff/driver.js
+++ drivers/sonoff/driver.js
@@ -53,35 +53,6 @@ module.exports = class SonoffDriver extends Homey.Driver {
     });
   }
 
-  onPairListDevices(data, callback) {
-    const pairDevices = devices => {
-      return callback(null, devices.map(device => {
-        let name = `${ device.data.model }@${ device.deviceId} (ROM ${ device.data.romVersion })`;
-        this.log('[pairing] got pairing request from', name);
-        return {
-          name,
-          data : {
-            id       : device.id,
-            deviceId : device.deviceId,
-            apiKey   : device.apiKey,
-            data     : device.data
-          }
-        }
-      }));
-    }
-
-    // Check if the manager is managing any unpaired devices.
-    let unpaired = this.manager.unpairedDevices();
-    if (unpaired.length) {
-      this.log(`[pairing] got ${ unpaired.length } device(s) ready to pair`);
-      pairDevices(unpaired);
-    } else {
-      // Wait for first device to announce itself.
-      this.log('[pairing] waiting for device to pair');
-      this.manager.once('new_device', device => pairDevices([ device ]));
-    }
-  }
-
   onDeleted(device) {
     this.log('[driver] device got deleted', device.getName());
     this.manager.unregisterDevice(device.getDeviceId());
