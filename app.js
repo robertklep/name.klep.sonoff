f4a1b43669ea8ffb230c920571712de80d1ac6c9
Robert Klep <robert@klep.name>
2018-02-16 09:56:59 +0100

    Allow different debugging tools to be enabled separately

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const Homey = require('homey');
 
-if (Homey.env.DEBUG && Homey.env.SYSLOG_HOST) {
+if (Homey.env.SYSLOG_HOST) {
   require('homey-syslog')();
 }
 
 module.exports = class SonoffApp extends Homey.App {
   onInit() {
     this.log('SonoffApp is running...');
-    if (Homey.env.DEBUG) {
+    if (Homey.env.INSPECTOR) {
       require('inspector').open(9229, '0.0.0.0');
+    }
+    if (Homey.env.UPLOADER) {
+      require('homey-app-upload-lib')(this.manifest);
     }
   }
 }
