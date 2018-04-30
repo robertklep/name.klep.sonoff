2d7d7903cbd7cb25b34394a8d7f73d4e4e0b94eb
Robert Klep <robert@klep.name>
2018-04-30 10:50:31 +0200

    Added Tasmota overview to Settings

--- api.js
+++ api.js
@@ -22,9 +22,14 @@ module.exports = [
     fn     : async (args, callback) => {
       try {
         let devices = await Homey.app.apiListTasmotaDevices();
+        for (let device of devices) {
+          device._ipAddress = await device.getIpAddress();
+        }
         return callback(null, devices.map(device => ({
-          id   : device.getData().id,
-          name : device.getName(),
+          id        : device.getData().id,
+          name      : device.getName(),
+          topic     : device.getTopic(),
+          ipAddress : device._ipAddress,
         })));
       } catch(err) {
         return callback(err);
